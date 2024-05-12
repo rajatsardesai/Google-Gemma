4c4172885583a20e2b438a87cb3e4a28e4a888c4
Rajat Sardesai <rajatsardesai@gmail.com>
2024-05-12 23:50:18 +0530

    Updated vite config file for Heroku Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Google-Gemma/",
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  },
+  base: "/",
   plugins: [react()],
 })
