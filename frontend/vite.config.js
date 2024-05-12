8b5df6e4ba58326837a1fae7dcac7c6dfd89670c
Rajat Sardesai <rajatsardesai@gmail.com>
2024-05-13 00:35:12 +0530

    Removed dist and update vit config file for proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://google-gemma.onrender.com/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
