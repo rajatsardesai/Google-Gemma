d3e31276db9c9d472b35880c56bb8cd8b67fd58c
Rajat Sardesai <rajatsardesai@gmail.com>
2024-05-12 22:48:34 +0530

    Prepare for Heroku Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.APIKEY; // Accessing API key from environment variable
 
 // Serve the static files from the frontend build directory
